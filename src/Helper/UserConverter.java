3a00e1ac03bdd344a09120ffdb3fc49efa426f99
awo-dev <anders@wiberg.tech>
2018-01-30 12:57:50 +0100

    Wrapper framework done

---
+++ src/Helper/UserConverter.java
@@ -0,0 +1,36 @@
+package Helper;
+import brugerautorisation.data.Bruger;
+import dto.User;
+
+public class UserConverter {
+    public static Bruger UserToBruger(User user) {
+        Bruger bruger = new Bruger();
+
+        bruger.brugernavn = user.getUsername();
+        bruger.email = user.getEmail();
+        bruger.sidstAktiv = user.getLastActive();
+        bruger.campusnetId = user.getCampusnetId();
+        bruger.fornavn = user.getFirstname();
+        bruger.efternavn = user.getLastname();
+        bruger.adgangskode = user.getPassword();
+        bruger.ekstraFelter = user.getExtraFields();
+
+        return bruger;
+    }
+
+    public static User BrugerToUser(Bruger bruger) {
+        User user = new User();
+
+        user.setUsername(bruger.brugernavn);
+        user.setEmail(bruger.email);
+        user.setLastActive(bruger.sidstAktiv);
+        user.setCampusnetId(bruger.campusnetId);
+        user.setStudyRetning(bruger.studeretning);
+        user.setFirstname(bruger.fornavn);
+        user.setLastname(bruger.efternavn);
+        user.setPassword(bruger.adgangskode);
+        user.setExtraFields(bruger.ekstraFelter);
+
+        return user;
+    }
+}
