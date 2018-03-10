e4ed5ab111bb38ce4d1ec61bffbf21befa47a26d
Anders W. Olsen <anders@wiberg.tech>
2018-03-10 12:08:27 +0100

    Revert "Add version 1.3.3"

---
+++ src/main/java/authwrapper/transport/IUserAdministration.java
@@ -0,0 +1,12 @@
+package authwrapper.transport;
+
+import authwrapper.dto.User;
+
+public interface IUserAdministration {
+    User authenticateUser(String username, String password) throws AuthenticationException, ConnectivityException;
+    User changePassword(String username, String oldPassword, String newPassword) throws AuthenticationException, ConnectivityException;
+    void sendEmail(String username, String password, String subject, String body) throws AuthenticationException, ConnectivityException;
+    void sendForgottenPasswordEmail(String username, String message) throws ConnectivityException;
+    void setExtraField(String username, String password, String fieldName, Object value) throws AuthenticationException, ConnectivityException;
+    Object getExtraField(String username, String password, String fieldName) throws AuthenticationException, ConnectivityException;
+}
